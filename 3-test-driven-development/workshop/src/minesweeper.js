1ee5eace8e71e9adb18cf92cb0efaf62aaf9cc1a
samy <samy_leni@hotmail.fr>
2022-04-28 11:46:42 +0200

    feat: algo board

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -1,19 +1,36 @@
 class Minesweeper {
-    constructor(n, m) {
-        this.n = n;
-        this.m = m;
-        this.input = [];
-        this.input.push(`${n} ${m}`);
-        for (let i = 0; i < n; i++) {
-            let row = '';
-            for (let j = 0; j < m; j++) {
-                row += Math.random() > .9 ? '*' : '.';
+    constructor(input) {
+        this.input = !input ? this.createRandomInput() : input;
+        this.output = this.getOutput();
+    }
+    
+    createRandomInput = () => {
+        const input = [];
+        
+        const boardCount = Math.ceil(Math.random() * 3);
+        for (let a = 0; a < boardCount; a++) {
+            const n = Math.ceil(Math.random() * 100);
+            const m = Math.ceil(Math.random() * 100);
+            input.push(`${n} ${m}`);
+            for (let i = 0; i < n; i++) {
+                let row = '';
+                for (let j = 0; j < m; j++) row += Math.random() > .9 ? '*' : '.';
+                input.push(row);
             }
-            this.input.push(row);
         }
+        return input;
+    }
+
+    getOutput = () => {
+        const [n, m] = game.input[0].split(' ');
+        const output = [];
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                
+            }
+        }
+        return output;
     }
 }
 
-module.exports = {
-    Minesweeper
-}
+module.exports = { Minesweeper }
