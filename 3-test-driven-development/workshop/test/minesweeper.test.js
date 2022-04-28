fdb120995ef75f441ff376df8cb5e3e88c4f4af2
samy <samy_leni@hotmail.fr>
2022-04-28 13:07:59 +0200

    Acceptance test input

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,28 +1,59 @@
 const { Minesweeper } = require("../src/minesweeper");
 
 describe("tests", () => {
-    test("board count", () => {
-        const game = new Minesweeper();
+    test("Acceptance test input", () => {
+        const game = new Minesweeper([
+            '4 4',
+            '*...',
+            '....',
+            '.*..',
+            '....',
+            '3 5',
+            '**...',
+            '.....',
+            '.*...',
+            '0 0'
+        ]);
 
-        const [n, m] = game.input[0].split(' ');
-        expect(n).toEqual(game.n.toString());
-        expect(m).toEqual(game.m.toString());
-        expect(game.input.length).toEqual(parseInt(n) + 1);
-        for (let i = 0; i < n; i++) {
-            expect(game.input[i + 1].length).toEqual(parseInt(m));
-        }
+        const result = [
+            'Field #1:',
+            '*100',
+            '2210',
+            '1*10',
+            '1110',
+            'Field #2:',
+            '**100',
+            '33200',
+            '1*100'
+        ];
+
+        game.output.forEach((row, i) => {
+            expect(row).toEqual(result[i]);
+        });
     });
 
-    test("Game must contains only stars, dots, and numbers", () => {
-        const game = new Minesweeper();
-        const regex = /[\d|*|.]*/; // check that row contains only digits, stars, or dot in game
+    // test("board count", () => {
+    //     const game = new Minesweeper();
 
-        game.input.forEach((row, i) => {
-            if (!i) return;
+    //     const [n, m] = game.input[0].split(' ');
+    //     expect(n).toEqual(game.n.toString());
+    //     expect(m).toEqual(game.m.toString());
+    //     expect(game.input.length).toEqual(parseInt(n) + 1);
+    //     for (let i = 0; i < n; i++) {
+    //         expect(game.input[i + 1].length).toEqual(parseInt(m));
+    //     }
+    // });
 
-            if (row.includes(' ') === true) return;
-            const matchingRegex = row.match(regex)[0];
-            expect(matchingRegex.length).toEqual(row.length)
-        })
-    });
+    // test("Game must contains only stars, dots, and numbers", () => {
+    //     const game = new Minesweeper();
+    //     const regex = /[\d|*|.]*/; // check that row contains only digits, stars, or dot in game
+
+    //     game.input.forEach((row, i) => {
+    //         if (!i) return;
+
+    //         if (row.includes(' ') === true) return;
+    //         const matchingRegex = row.match(regex)[0];
+    //         expect(matchingRegex.length).toEqual(row.length)
+    //     })
+    // });
 });
