0dfca849eb806d0edc425fbb4fa2c7cc70d0105b
AlexSuchot <alexandre.suchot@ynov.com>
2022-04-28 13:15:03 +0200

    test: uncomment working test

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -44,18 +44,18 @@ describe("tests", () => {
     //     }
     // });
 
-    // test("Game must contains only stars, dots, and numbers", () => {
-    //     const game = new Minesweeper();
-    //     const regex = /[\d|*|.]*/; // check that row contains only digits, stars, or dot in game
+    test("Game must contains only stars, dots, and numbers", () => {
+        const game = new Minesweeper();
+        const regex = /[\d|*|.]*/; // check that row contains only digits, stars, or dot in game
 
-    //     game.input.forEach((row, i) => {
-    //         if (!i) return;
+        game.input.forEach((row, i) => {
+            if (!i) return;
 
-    //         if (row.includes(' ') === true) return;
-    //         const matchingRegex = row.match(regex)[0];
-    //         expect(matchingRegex.length).toEqual(row.length)
-    //     })
-    // });
+            if (row.includes(' ') === true) return;
+            const matchingRegex = row.match(regex)[0];
+            expect(matchingRegex.length).toEqual(row.length)
+        })
+    });
 
     test("Game length (n,m) must be between 0 and 100 excluded", () => {
         const game = new Minesweeper();
