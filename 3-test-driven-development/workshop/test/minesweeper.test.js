f88c191ed40c6aaba6c591bfb56030e75c132930
AlexSuchot <alexandre.suchot@ynov.com>
2022-04-28 12:00:58 +0200

    test: check that row in game are valid

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,4 +1,4 @@
-const { Item, Shop, Minesweeper } = require("../src/minesweeper");
+const { Minesweeper } = require("../src/minesweeper");
 
 describe("tests", () => {
     test("board size", () => {
@@ -10,5 +10,18 @@ describe("tests", () => {
         for (let i = 0; i < n; i++) {
             expect(game.input[i + 1].length).toEqual(parseInt(m));
         }
+    });
+
+    test("Game must contains only stars, dots, and numbers", () => {
+        const game = new Minesweeper();
+        const regex = /[\d|*|.]*/; // check that row contains only digits, stars, or dot in game
+
+        game.input.forEach((row, i) => {
+            if (!i) return;
+
+            if (row.includes(' ') === true) return;
+            const matchingRegex = row.match(regex)[0];
+            expect(matchingRegex.length).toEqual(row.length)
+        })
     });
 });
