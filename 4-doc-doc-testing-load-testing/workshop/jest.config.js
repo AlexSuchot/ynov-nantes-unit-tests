881571391a91f1571099dde8d96430917c30eb94
samy <samy_leni@hotmail.fr>
2022-04-29 13:48:22 +0200

    jest config

---
+++ 4-doc-doc-testing-load-testing/workshop/jest.config.js
@@ -0,0 +1,10 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+    verbose: true,
+    testMatch: [
+        "**/*.test.js"
+    ],
+};
+
+module.exports = config;
